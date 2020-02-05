88003b8ad190093054adaac83a67b63986722530
Delink <delinkdesigns@gmail.com>
2020-02-05 19:50:33 +0300

    added model for the proverb

---
+++ app/src/main/java/com/example/thimo/models/Thimo.java
@@ -0,0 +1,18 @@
+package com.example.thimo.models;
+
+public class Thimo {
+    private String proverb;
+
+    public Thimo() {}
+
+    public  Thimo(String proverb) {
+        this.proverb = proverb;
+    }
+
+    public String getProverb() {
+        return proverb;
+    }
+    public void setProverb(String proverb) {
+        this.proverb = proverb;
+    }
+}
