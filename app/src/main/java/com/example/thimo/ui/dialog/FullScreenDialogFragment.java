5e88f2b3f02860830b0a5c40abc045b176f381e9
Delink <delinkdesigns@gmail.com>
2020-02-06 21:58:51 +0300

    added full screen dialog to show details

---
+++ app/src/main/java/com/example/thimo/ui/dialog/FullScreenDialogFragment.java
@@ -0,0 +1,56 @@
+package com.example.thimo.ui.dialog;
+
+
+import android.app.Dialog;
+import android.os.Bundle;
+
+import androidx.appcompat.widget.Toolbar;
+import androidx.fragment.app.DialogFragment;
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.example.thimo.R;
+
+import java.util.Objects;
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class FullScreenDialogFragment extends DialogFragment {
+
+    public static  String TAG = "FullScreenDialog";
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setStyle(DialogFragment.STYLE_NORMAL, R.style.FullScreenDialogStyle);
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_full_screen_dialog, container, false);
+
+        Toolbar toolbar = view.findViewById(R.id.toolbar);
+        toolbar.setNavigationIcon(R.drawable.ic_close_white_24dp);
+//        toolbar.setNavigationOnClickListener(view1 -> cancelUpload());
+        toolbar.setTitle("Thimo Dialog");
+
+        // Inflate the layout for this fragment
+        return view;
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+        Dialog dialog = getDialog();
+        if (dialog != null) {
+            int width = ViewGroup.LayoutParams.MATCH_PARENT;
+            int height = ViewGroup.LayoutParams.MATCH_PARENT;
+            Objects.requireNonNull(dialog.getWindow()).setLayout(width, height);
+        }
+    }
+}
