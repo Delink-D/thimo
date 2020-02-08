ae2c040f084de7461265ecd305afa39e6193a83e
Delink <delinkdesigns@gmail.com>
2020-02-08 15:22:10 +0300

    remove of unused code

--- app/src/main/java/com/example/thimo/Adapters/CustomRecyclerAdapter.java
+++ app/src/main/java/com/example/thimo/Adapters/CustomRecyclerAdapter.java
@@ -1,7 +1,6 @@
 package com.example.thimo.Adapters;
 
 import android.content.Context;
-import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
