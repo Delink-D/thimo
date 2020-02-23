0c6014809a89a5c829e637440cadc6178de069b2
Delink <delinkdesigns@gmail.com>
2020-02-23 14:00:05 +0300

    added fragment to add proverbs

---
+++ app/src/main/java/com/example/thimo/ui/dialog/AddProverbDialogFragment.java
@@ -0,0 +1,66 @@
+package com.example.thimo.ui.dialog;
+
+import android.app.Dialog;
+import android.os.Bundle;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.appcompat.widget.Toolbar;
+import androidx.fragment.app.DialogFragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.example.thimo.R;
+
+import java.util.Objects;
+
+public class AddProverbDialogFragment extends DialogFragment {
+    public static String TAG = "AddProverbDialog";
+
+    public static AddProverbDialogFragment newInstance() {
+        return new AddProverbDialogFragment();
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState){
+        super.onCreate(savedInstanceState);
+        setStyle(DialogFragment.STYLE_NORMAL, R.style.FullScreenDialogStyle);
+    }
+
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container,
+                             @Nullable Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.add_proverb_dialog_fragment, container, false);
+
+        Toolbar toolbar = view.findViewById(R.id.toolbar);
+        toolbar.setNavigationIcon(R.drawable.ic_arrow_back_white_24dp);
+        toolbar.setNavigationOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                dismiss();
+            }
+        });
+        toolbar.setTitle("Add a proverb");
+
+        return view;
+    }
+
+    @Override
+    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+    }
+    @Override
+    public void onStart() {
+        super.onStart();
+        Dialog dialog = getDialog();
+        if (dialog != null) {
+            int width = ViewGroup.LayoutParams.MATCH_PARENT;
+            int height = ViewGroup.LayoutParams.MATCH_PARENT;
+            Objects.requireNonNull(dialog.getWindow()).setLayout(width, height);
+        }
+    }
+
+
+}
